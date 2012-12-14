91c101ecce8d61398aece8029c0285838a6f0d35
Matthias Mengel <mengel@pik-potsdam.de>
2012-12-14 20:58:58 +0100

    submit script.

---
+++ submit.sh
@@ -0,0 +1,10 @@
+
+# Run this script from your shell to submit your IMOA calc to the cluster.
+# Choose the number of cores with -t.
+# The program causes heavy file I/O when used with many cores,
+# i'd suggest to use not more than 48.
+# Add export PATH=$PATH:/iplex/01/sys/applications/python/ibin to your .profile or .bashrc
+
+#llrun -t 4 -c medium -g isimip -N Diffusion cpy runDiffusion.py
+llrun -t 8 -c short -g isimip -N Diffusion cpy prepareAndDiffuse.py
+
